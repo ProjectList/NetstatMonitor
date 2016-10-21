e22581f25d48d5561f6112c1b7f4ec83ffe3557c
ZDT@HP <dev@dthappy.com>
2016-10-21 15:02:36 +0800

    Init with netstat monitor

---
+++ NetstatMonitor/NetstatForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace NetstatMonitor
+{
+    partial class NetstatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.filterTb = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.monitorGirdview = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkCount = new System.Windows.Forms.Label();
+            this.TimeStamp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Protocol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LocalAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RemoteAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.State = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.monitorGirdview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter Condition:";
+            // 
+            // filterTb
+            // 
+            this.filterTb.Location = new System.Drawing.Point(125, 10);
+            this.filterTb.Name = "filterTb";
+            this.filterTb.Size = new System.Drawing.Size(401, 21);
+            this.filterTb.TabIndex = 1;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(543, 8);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(127, 23);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Start Monitor";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // monitorGirdview
+            // 
+            this.monitorGirdview.AllowUserToDeleteRows = false;
+            this.monitorGirdview.AllowUserToOrderColumns = true;
+            this.monitorGirdview.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.monitorGirdview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.monitorGirdview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TimeStamp,
+            this.PID,
+            this.Protocol,
+            this.LocalAddress,
+            this.RemoteAddress,
+            this.State});
+            this.monitorGirdview.Location = new System.Drawing.Point(9, 83);
+            this.monitorGirdview.Name = "monitorGirdview";
+            this.monitorGirdview.ReadOnly = true;
+            this.monitorGirdview.RowTemplate.Height = 23;
+            this.monitorGirdview.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.monitorGirdview.Size = new System.Drawing.Size(661, 369);
+            this.monitorGirdview.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Current tcp link count by filter condition:";
+            // 
+            // linkCount
+            // 
+            this.linkCount.AutoSize = true;
+            this.linkCount.Location = new System.Drawing.Point(276, 50);
+            this.linkCount.Name = "linkCount";
+            this.linkCount.Size = new System.Drawing.Size(11, 12);
+            this.linkCount.TabIndex = 5;
+            this.linkCount.Text = "0";
+            // 
+            // TimeStamp
+            // 
+            this.TimeStamp.HeaderText = "TimeStamp";
+            this.TimeStamp.Name = "TimeStamp";
+            this.TimeStamp.ReadOnly = true;
+            // 
+            // PID
+            // 
+            this.PID.FillWeight = 50F;
+            this.PID.HeaderText = "PID";
+            this.PID.Name = "PID";
+            this.PID.ReadOnly = true;
+            this.PID.Width = 50;
+            // 
+            // Protocol
+            // 
+            this.Protocol.FillWeight = 60F;
+            this.Protocol.HeaderText = "Protocol";
+            this.Protocol.Name = "Protocol";
+            this.Protocol.ReadOnly = true;
+            this.Protocol.Width = 60;
+            // 
+            // LocalAddress
+            // 
+            this.LocalAddress.FillWeight = 150F;
+            this.LocalAddress.HeaderText = "Local Address";
+            this.LocalAddress.Name = "LocalAddress";
+            this.LocalAddress.ReadOnly = true;
+            this.LocalAddress.Width = 150;
+            // 
+            // RemoteAddress
+            // 
+            this.RemoteAddress.FillWeight = 150F;
+            this.RemoteAddress.HeaderText = "Remote Address";
+            this.RemoteAddress.Name = "RemoteAddress";
+            this.RemoteAddress.ReadOnly = true;
+            this.RemoteAddress.Width = 150;
+            // 
+            // State
+            // 
+            this.State.HeaderText = "State";
+            this.State.Name = "State";
+            this.State.ReadOnly = true;
+            // 
+            // NetstatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 485);
+            this.Controls.Add(this.linkCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.monitorGirdview);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.filterTb);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(700, 523);
+            this.Name = "NetstatForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Netstat Monitor";
+            ((System.ComponentModel.ISupportInitialize)(this.monitorGirdview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox filterTb;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.DataGridView monitorGirdview;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label linkCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimeStamp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Protocol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LocalAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RemoteAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn State;
+    }
+}
+
---
+++ NetstatMonitor/NetstatForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetstatMonitor
+{
+    public partial class NetstatForm : Form
+    {
+        private bool _isMonitor ;
+        private string logs;
+        private int currentLogCount;
+        public NetstatForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (startBtn.Text == "Start Monitor")
+            {
+                _isMonitor = true;
+                Monitor(filterTb.Text);
+                startBtn.Text = "Stop Monitor";
+            }
+            else
+            {
+                _isMonitor = false;
+                filterTb.Text = "";
+                startBtn.Text = "Start Monitor";
+            }
+            //GetNetstatByCmd();
+        }
+
+        private async void Monitor(String filter)
+        {
+            while (_isMonitor)
+            {
+                MonitorNetstat(filter);
+                await Task.Delay(1000);
+            }
+            WriteLog(true);
+        }
+
+        private void MonitorNetstat(String filter)
+        {
+            //string cmd = "/c echo %date% %time% >> log.txt && netstat -an >> log.txt"; 
+            if (String.IsNullOrEmpty(filter))
+            {
+                MessageBox.Show("Filter cannot be null");
+                return;
+            }
+            var cmd = "/c netstat -anop tcp | find \"" + filter + "\"";
+            var proc = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "cmd.exe",
+                    Arguments = cmd,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true
+                }
+            };
+            proc.Start();
+            var output = proc.StandardOutput.ReadToEnd();
+            var currentTime = DateTime.Now.ToLongTimeString();
+            var lines = Regex.Split(output, "\r\n");
+            linkCount.Text = (lines.Length - 1).ToString();
+            foreach (var tcpInfo in lines.Select(line => Regex.Split(line, "\\s+")).Where(tcpInfo => tcpInfo.Length == 6))
+            {
+                tcpInfo[0] = currentTime;
+                monitorGirdview.Rows.Add(tcpInfo[0], tcpInfo[5], tcpInfo[1], tcpInfo[2], tcpInfo[3], tcpInfo[4]);
+                AddToLog(tcpInfo[0], tcpInfo[5], tcpInfo[1], tcpInfo[2], tcpInfo[3], tcpInfo[4]);
+            }
+            monitorGirdview.FirstDisplayedScrollingRowIndex = monitorGirdview.RowCount - 1;
+        }
+
+        private void AddToLog(params string[] logInfo)
+        {
+            string log = String.Join("\t", logInfo);
+            logs += (log + "\r\n");
+            ++currentLogCount;
+            WriteLog();
+        }
+
+        private void WriteLog(bool flush = false)
+        {   
+            if (currentLogCount < 1000 && !flush) return;
+            using (var streamWriter = new StreamWriter("Log.txt", true))
+            {
+                streamWriter.WriteLine(logs);
+                streamWriter.Flush();
+            }
+            logs = "";
+            currentLogCount = 0;
+        }
+
+        private static void GetNetstatByCmd()
+        {
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
+            cmdStartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            cmdStartInfo.RedirectStandardOutput = true;
+            cmdStartInfo.RedirectStandardError = true;
+            cmdStartInfo.RedirectStandardInput = true;
+            cmdStartInfo.UseShellExecute = false;
+            cmdStartInfo.CreateNoWindow = true;
+
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo = cmdStartInfo;
+            cmdProcess.ErrorDataReceived += cmd_Error;
+            cmdProcess.OutputDataReceived += cmd_DataReceived;
+            cmdProcess.EnableRaisingEvents = true;
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+            cmdProcess.BeginErrorReadLine();
+
+            cmdProcess.StandardInput.WriteLine("netstat -an | find \"80\"");
+            cmdProcess.StandardInput.WriteLine("exit"); //Execute exit.
+            cmdProcess.WaitForExit();
+        }
+
+        static void cmd_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine("Output from other process");
+            Console.WriteLine(e.Data);
+        }
+
+        static void cmd_Error(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine("Error from other process");
+            Console.WriteLine(e.Data);
+        }
+        private static void getActiveTcp()
+        {
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] endPoints = ipProperties.GetActiveTcpListeners();
+            TcpConnectionInformation[] tcpConnections =
+                ipProperties.GetActiveTcpConnections();
+
+            foreach (TcpConnectionInformation info in tcpConnections)
+            {
+                Console.WriteLine("Local: {0}:{1}\nRemote: {2}:{3}\nState: {4}\n",
+                    info.LocalEndPoint.Address, info.LocalEndPoint.Port,
+                    info.RemoteEndPoint.Address, info.RemoteEndPoint.Port,
+                    info.State.ToString());
+            }
+        }
+    }
+}
